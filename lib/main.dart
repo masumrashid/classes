e36284028edcec87331a0a9bd58e56be1cdd4021
masumrashid <razzshikder@gmail.com>
2025-01-01 23:16:43 +0600

    Live twst 01

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,18 @@
-abstract class Role {
-  void displayRole();
-}
-
-
-class Person implements Role {
-  String name;
-  int age;
-  String address;
-
-  Person(this.name, this.age, this.address);
-
-  void displayRole() {
-
-  }
-}
-
-
-class Student extends Person {
-  String studentID;
-  String grade;
-  List<int> courseScores;
-
-  Student(String name, int age, String address, this.studentID, this.grade, this.courseScores)
-      : super(name, age, address);
-
-  double calculateAverageScore() {
-    return courseScores.isEmpty
-        ? 0.0
-        : courseScores.reduce((a, b) => a + b) / courseScores.length;
-  }
-
-  void displayRole() {
-    print("Role: Student");
-  }
-
-  void displayStudentInfo() {
-    displayRole();
-    print("Name: $name");
-    print("Age: $age");
-    print("Address: $address");
-    print("Average Score: ${calculateAverageScore().toStringAsFixed(1)}");
-  }
-}
-
-
-class Teacher extends Person {
-  String teacherID;
-  List<String> coursesTaught;
-
-  Teacher(String name, int age, String address, this.teacherID, this.coursesTaught)
-      : super(name, age, address);
-
-
-  void displayRole() {
-    print("Role: Teacher");
-  }
-
-  void displayTeacherInfo() {
-    displayRole();
-    print("Name: $name");
-    print("Age: $age");
-    print("Address: $address");
-    print("Courses Taught:");
-    for (var course in coursesTaught) {
-      print("- $course");
-    }
-  }
-}
-
+import 'dart:io';
 void main() {
-//Student instance
-  var student = Student(
-    "John Doe",
-    20,
-    "123 Main St",
-    "S123",
-    "A",
-    [90, 85, 82],
-  );
+  triangle();
+}
 
-  //Teacher instance
-  var teacher = Teacher(
-    "Mrs. Smith",
-    35,
-    "456 Oak St",
-    "T456",
-    ["Math", "English", "Bangla"],
-  );
+void triangle() {
+ print('Enter the base of the triangle: ');
+  double base = double.parse(stdin.readLineSync()!);
 
-  print("Student Information:");
-  student.displayStudentInfo();
 
-  print("\nTeacher Information:");
-  teacher.displayTeacherInfo();
+  print('Enter the height of the triangle: ');
+  double height = double.parse(stdin.readLineSync()!);
+
+  double area = 0.5 * base * height;
+
+
+  print('The area of the triangle is: $area');
 }
